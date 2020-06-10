a750d15830038771ae8fb10c8680ba2fb660451d
Jiho Lim <very725@naver.com>
2020-06-11 00:39:46 +0900

    first commit

---
+++ blogproject1/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+import blog.views
+import portfolio.views
+from django.conf import settings # settings에 있는 url
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', blog.views.home, name="home"),
+    path('blog/', include('blog.urls')),
+    path('portfolio/', portfolio.views.portfolio, name="portfolio"),
+    path('accounts/', include('accounts.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
